8b358140edd7dcce2040f5c9191b8a00861deff9
pavank-v <pavankumar.v1301@gmail.com>
2026-02-07 12:50:00 +0530

    Allowing player movement

--- public/index.js
+++ public/index.js
@@ -1,13 +1,21 @@
 const mapImage = new Image();
+const santaImage = new Image();
+
 mapImage.src = "./snowy-sheet.png";
+santaImage.src = "./santa.png";
 
 const canvasEle = document.getElementById("canvas");
 canvasEle.width = window.innerWidth;
 canvasEle.height = window.innerHeight;
-const canvas = canvasEle.getContext('2d');
 
+const canvas = canvasEle.getContext('2d');
 const socket = io("ws://localhost:3000");
+
+const TILES_IN_ROW = 8;
+const TILE_SIZE = 16;
+
 let map = [[]];
+let players = [];
 
 socket.on("connect", () => {
 	console.log("connected")
@@ -17,11 +25,45 @@ socket.on("map", (loadedMap) => {
 	map = loadedMap;
 });
 
-const TILES_IN_ROW = 8;
-const TILE_SIZE = 16;
+socket.on("players", (serverPlayers) => {
+	players = serverPlayers;
+});
+
+const inputs = {
+	up: false,
+	down: false,
+	right: false,
+	left: false
+};
+
+window.addEventListener("keydown", (e) => {
+	if (e.key === 'a' || e.key === "ArrowLeft")
+		inputs["left"] = true;
+	else if (e.key === 's' || e.key === "ArrowDown")
+		inputs["down"] = true;
+	else if (e.key === 'w' || e.key === "ArrowUp")
+		inputs["up"] = true;
+	else if (e.key === 'd' || e.key === "ArrowRight")
+		inputs["right"] = true;
+
+	socket.emit("inputs", inputs);
+});
+
+window.addEventListener("keyup", (e) => {
+	if (e.key === 'a' || e.key === "ArrowLeft")
+		inputs["left"] = false;
+	else if (e.key === 's' || e.key === "ArrowDown")
+		inputs["down"] = false;
+	else if (e.key === 'w' || e.key === "ArrowUp")
+		inputs["up"] = false;
+	else if (e.key === 'd' || e.key === "ArrowRight")
+		inputs["right"] = false;
+
+	socket.emit("inputs", inputs);
+});
 
 const loop = () => {
-	canvas.clearRect(0, 0, canvas.width, canvas.height);
+	canvas.clearRect(0, 0, canvasEle.width, canvasEle.height);
 
 	for (let r = 0; r < map.length; r++) {
 		for (let c = 0; c < map[0].length; c++) {
@@ -42,7 +84,11 @@ const loop = () => {
 			);
 		}
 	}
-	
+
+	for (const player of players){
+		canvas.drawImage(santaImage, player.x, player.y);
+	}
+
 	window.requestAnimationFrame(loop);
 }
 
--- src/index.js
+++ src/index.js
@@ -7,17 +7,68 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+const TICK_RATE = 30;
+const SPEED = 5;
+let players = [];
+let inputsMap = {};
+
+const tick = () => {
+  for (const player of players) {
+    const input = inputsMap[player.id];
+    console.log(player, input)
+
+    if (input.up)
+      player.y -= SPEED;
+    else if(input.down)
+      player.y += SPEED;
+
+    if(input.right)
+      player.x += SPEED;
+    else if(input.left)
+      player.x -= SPEED;
+  }
+
+  io.emit("players", players);
+}
+
 const main = async () => {
   const map2D = await LoadMap();
 
   io.on("connect", (socket) => {
     console.log("user connected", socket.id);
 
-    socket.emit("map", map2D);
-  })
+    inputsMap[socket.id] = {
+      up: false,
+      down: false,
+      left: false,
+      right: false,
+    };
 
+    players.push({
+      id: socket.id,
+      x: 0,
+      y: 0,
+    });
+
+    socket.emit("map", map2D);
+
+    socket.on("inputs", (inputs) => {
+      inputsMap[socket.id] = inputs;
+    });
+
+    socket.on("disconnect", () => {
+      players = players.filter((player) => {
+        player.id != socket.id;
+      })
+    })
+
+  });
+
+  console.log("players: ", players)
   app.use(express.static("public"));
   httpServer.listen(3000);
+
+  setInterval(tick, 1000 / TICK_RATE);
 }
 
 main()
